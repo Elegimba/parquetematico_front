7c701bfbff72248972badaed65415ec612d1716c
YagoGV <yagogv@gmail.com>
2024-12-10 18:35:50 +0100

    atascado en attraction-details.ts dice que el id de la url es undefined

--- src/app/interfaces/iattraction.interface.ts
+++ src/app/interfaces/iattraction.interface.ts
@@ -1,9 +1,9 @@
 export interface IAttraction {
     id: number;
     capacity: number;
-    min_height: string;
-    average_duration: string;
-    wait_time: string;
+    min_height: number;
+    average_duration: number;
+    wait_time: number;
     functional: boolean;
     name: string;
 }
--- src/app/pages/attraction-details/attraction-details.component.ts
+++ src/app/pages/attraction-details/attraction-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, Input } from '@angular/core';
 import { IAttraction } from '../../interfaces/iattraction.interface';
 import { AttractionsService } from '../../services/attractions.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-attraction-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './attraction-details.component.html',
   styleUrl: './attraction-details.component.css'
 })
@@ -13,9 +14,16 @@ export class AttractionDetailsComponent {
 
   attraction: IAttraction | null = null;
 
-  @Input() attractionId: string = '';
+  @Input() attractionId: number = 0;
 
   attractionsService = inject(AttractionsService);
 
-  
+  async ngOnInit() {
+    try {
+      this.attraction = await this.attractionsService.getById(this.attractionId);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 }
