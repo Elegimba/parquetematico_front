32a96826ee7da8a5d7e67b9a2bfdaa8bec87e57f
YagoGV <yagogv@gmail.com>
2024-12-12 10:13:47 +0100

    interface, getAll y getById

---
+++ src/app/services/comments.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IComment } from '../interfaces/icomment.interface';
+import { lastValueFrom } from 'rxjs';
+
+type createComment = {  }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsService {
+
+  private httpClient = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/comments`
+
+  getAll(): Promise<IComment[]> {
+    return lastValueFrom(
+      this.httpClient.get<IComment[]>(this.baseUrl)
+    )
+  }
+
+  getById(commentId: number): Promise<IComment> {
+    return lastValueFrom(
+      this.httpClient.get<IComment>(`${this.baseUrl}/${commentId}`)
+    )
+  }
+
+  /* create(body: createComment) */
+}
