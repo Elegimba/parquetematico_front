2303b0eb85e0a5ebe005ad97f6a7ae5d9b5cb400
YagoGV <yagogv@gmail.com>
2024-12-19 11:04:22 +0100

    update component

---
+++ src/app/components/update-comment/update-comment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-comment',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-comment.component.html',
+  styleUrl: './update-comment.component.css'
+})
+export class UpdateCommentComponent {
+
+}
